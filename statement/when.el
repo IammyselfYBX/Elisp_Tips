33d3ce338733a80ddb9df9bc41451f530c21244e
YBX <226323@111>
2021-02-24 09:06:47 +0800

    mv some file in Folder

--- statement/when.el
+++
@@ -1,5 +0,0 @@
-;;相比if when是只有 true 才执行为假不执行
-(setq my-cond nil)
-(when my-cond
-  (message "YBX is the most handsome man in NCEPU")
-  (message "YBX is the most briliant man in NCEPU"))
--- statement/while.el
+++
@@ -1,5 +0,0 @@
-;;while循环语句
-(setq i 0)
-(while (< i 10)
-  (message "i=%s" i)
-  (setq i (+ 1 i)))
